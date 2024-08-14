2d3feeef26fd91b733d798935d1e57cf8b097253
ruzstock.ir <ali16mar15couples@gmail.com>
2024-08-14 13:08:36 +0330

    camunda config to start engine

---
+++ src/main/java/org/camunda/bpm/getstarted/springbootcamundakeycloakexample/CamundaConfig.java
@@ -0,0 +1,18 @@
+package org.camunda.bpm.getstarted.springbootcamundakeycloakexample;
+
+import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.camunda.bpm.engine.impl.cfg.StandaloneInMemProcessEngineConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CamundaConfig {
+
+    @Bean
+    public ProcessEngineConfigurationImpl processEngineConfiguration() {
+        StandaloneInMemProcessEngineConfiguration config = new StandaloneInMemProcessEngineConfiguration();
+        config.setJobExecutorActivate(true);
+        // Additional configuration
+        return config;
+    }
+}
